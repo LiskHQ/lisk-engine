24c9374b8bcea8697ae0bdf2cc2828b2d796b5aa
hatef <rahmani.hatef@gmail.com>
2023-02-16 20:01:50 +0300

    :seedling: Add sample ConnGater with expireTime

--- pkg/p2p/v2/conngater.go
+++ pkg/p2p/v2/conngater.go
@@ -3,10 +3,71 @@ package p2p
 import (
 	"fmt"
 	"net"
+	"sync"
+	"time"
 
 	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 )
+
+type Gater struct {
+	gc          *conngater.BasicConnectionGater
+	blockedPeer map[peer.ID]int64
+	expireTime  time.Duration
+	interval    time.Duration
+	mutex       sync.RWMutex
+}
+
+func newGater(expireTime, interval time.Duration) (*Gater, error) {
+	gc, err := conngater.NewBasicConnectionGater(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Gater{
+		gc:          gc,
+		blockedPeer: make(map[peer.ID]int64),
+		expireTime:  expireTime,
+		interval:    interval,
+		mutex:       sync.RWMutex{},
+	}, nil
+}
+
+func (g *Gater) addPeer(pid peer.ID) error {
+	g.mutex.Lock()
+	defer g.mutex.Unlock()
+
+	err := g.gc.BlockPeer(pid)
+	if err != nil {
+		return err
+	}
+	g.blockedPeer[pid] = time.Now().Unix() + int64(g.expireTime.Seconds())
+
+	return nil
+}
+
+func (g *Gater) start() {
+	go func() {
+		for range time.Tick(g.interval) {
+			for p, t := range g.blockedPeer {
+				if time.Now().Unix() > t {
+					err := g.gc.UnblockPeer(p)
+					if err != nil {
+						panic(err)
+					}
+					g.mutex.Lock()
+					delete(g.blockedPeer, p)
+					g.mutex.Unlock()
+				}
+			}
+		}
+	}()
+}
+
+func (g *Gater) connectionGater() *conngater.BasicConnectionGater {
+	return g.gc
+}
 
 // ConnectionGaterOption returns the ConnectionGater option of the libp2p which
 // is usable to reject incoming and outgoing connections based on blacklists.
--- pkg/p2p/v2/conngater_test.go
+++ pkg/p2p/v2/conngater_test.go
@@ -2,7 +2,9 @@ package p2p
 
 import (
 	"testing"
+	"time"
 
+	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 	"github.com/stretchr/testify/assert"
 )
@@ -27,4 +29,34 @@ func TestBlacklistIPs(t *testing.T) {
 	}
 	_, err = ConnectionGaterOption(cg, validIPs)
 	assert.Nil(err)
+}
+
+func TestConnGater_ExpireTime(t *testing.T) {
+	assert := assert.New(t)
+
+	g, err := newGater(time.Second*4, time.Second*2)
+	assert.Nil(err)
+	g.start()
+
+	pidA := peer.ID("A")
+	g.addPeer(pidA)
+
+	time.Sleep(time.Second)
+	pidB := peer.ID("B")
+	g.addPeer(pidB)
+	pidC := peer.ID("C")
+	g.addPeer(pidC)
+
+	gater := g.connectionGater()
+	assert.Equal(3, len(gater.ListBlockedPeers()))
+	time.Sleep(time.Second)
+	pidD := peer.ID("D")
+	g.addPeer(pidD)
+
+	time.Sleep(time.Second * 3)
+	assert.Equal(4, len(gater.ListBlockedPeers()))
+	time.Sleep(time.Second * 2)
+	assert.Equal(1, len(gater.ListBlockedPeers()))
+	time.Sleep(time.Second)
+	assert.Equal(0, len(gater.ListBlockedPeers()))
 }
