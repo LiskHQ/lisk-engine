2ccb1defa9efc9ca02266b06d0cefc56a04cd811
shuse2 <shus.toda@gmail.com>
2023-03-16 08:56:36 +0100

    :recycle: Fix format

--- pkg/codec/gen/internal/bindata.go
+++ pkg/codec/gen/internal/bindata.go
@@ -156,11 +156,13 @@ var _bindata = map[string]func() (*asset, error){
 // directory embedded in the file by go-bindata.
 // For example if you run go-bindata on data/... and data contains the
 // following hierarchy:
-//     data/
-//       foo.txt
-//       img/
-//         a.png
-//         b.png
+//
+//	data/
+//	  foo.txt
+//	  img/
+//	    a.png
+//	    b.png
+//
 // then AssetDir("data") would return []string{"foo.txt", "img"}
 // AssetDir("data/img") would return []string{"a.png", "b.png"}
 // AssetDir("foo.txt") and AssetDir("notexist") would return an error
@@ -193,8 +195,8 @@ type bintree struct {
 }
 
 var _bintree = &bintree{nil, map[string]*bintree{
-	"templates": &bintree{nil, map[string]*bintree{
-		"codec.tmpl": &bintree{templatesCodecTmpl, map[string]*bintree{}},
+	"templates": {nil, map[string]*bintree{
+		"codec.tmpl": {templatesCodecTmpl, map[string]*bintree{}},
 	}},
 }}
 
