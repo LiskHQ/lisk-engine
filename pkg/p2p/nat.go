74f48bee13ac6467a0d3660d6fa65ed5716f6838
shuse2 <shus.toda@gmail.com>
2024-01-10 15:33:45 +0100

    bug: fix golangci vertion

--- pkg/p2p/nat.go
+++ pkg/p2p/nat.go
@@ -5,7 +5,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/libp2p/go-libp2p"
+	libp2p "github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/core/event"
 	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/p2p/host/autonat"
